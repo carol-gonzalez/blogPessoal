cdc777aa89f70fc93cc4cba5d66e474e0f536220
carol-gonzalez <98carolinagonzalez@gmail.com>
2021-10-17 23:48:28 -0300

    atualizações

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -9,15 +10,41 @@ import { UserLogin } from '../model/UserLogin';
 })
 export class AuthService {
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('https://generationblogpessoalroseo.herokuapp.com/usuarios/logar', userLogin)
   }
 
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
+    return this.http.post<User>('https://generationblogpessoalroseo.herokuapp.com/usuarios/cadastrar', user)
+  }
+
+  atualizar(user: User): Observable<User>{
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+    return this.http.put<User>('https://generationblogpessoalroseo.herokuapp.com/usuarios/atualizar', user, this.token)
+  }
+
+  getByIdUser(id: number): Observable<User>{
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+    return this.http.get<User>(`https://generationblogpessoalroseo.herokuapp.com/usuarios/${id}`, this.token)
+  }
+
+  logado(){
+    let ok: boolean = false
+
+    if(environment.token != ''){
+      ok = true
+    }
+
+    return ok
   }
 }
